ac7fd9206dc4a6e15f851462a7f2deae5f07d363
madiiikkk <madik2212@gmail.com>
2026-05-24 22:27:32 +0500

    НАСТРОЙКА ПОД ХОСТ

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,25 @@ import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr()
+  ],
+  
+  preview: {
+    host: "0.0.0.0",
+    port: Number(process.env.PORT) || 8080, 
+    allowedHosts: [
+      "diplomfrontendlawly-production.up.railway.app",
+    ],
+  },
+
+  server: {
+    host: "0.0.0.0",
+    port: Number(process.env.PORT) || 8080, 
+    allowedHosts: [
+      "diplomfrontendlawly-production.up.railway.app",
+    ],
+  }
 })
