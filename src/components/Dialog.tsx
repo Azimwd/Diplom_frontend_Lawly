c18ec0db28b3c7787dda612c8e9f33b41ef46773
madiiikkk <madik2212@gmail.com>
2026-05-30 19:49:07 +0500

    themee v15

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -245,12 +245,14 @@ export default function Dialog({ messages, loading, isTyping, isStoped, onChoose
 
 												<div className="w-full">
 													<div className="flex justify-between text-sm mb-1">
-														<span className="text-[#f87171]">{t.lc}</span>
+														<span className="text-[#fc2727] dark:text-[#f87171]">
+															{t.lc}
+														</span>
 														<span className="text-white font-bold">{msg.lossRate}%</span>
 													</div>
 													<div className="w-full bg-[#2A2A2A] rounded-full h-2.5">
 														<div
-															className="bg-[#8c2828] h-2.5 rounded-full transition-all duration-1000"
+															className="bg-[#c13636] dark:bg-[#8c2828] h-2.5 rounded-full transition-all duration-1000"
 															style={{ width: `${msg.lossRate}%` }}
 														></div>
 													</div>
