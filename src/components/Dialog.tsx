d8d3132f7d67df1571e74238bed13c2ee93390bf
madiiikkk <madik2212@gmail.com>
2026-05-30 19:20:10 +0500

    themee v8

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -136,7 +136,7 @@ export default function Dialog({ messages, loading, isTyping, isStoped, onChoose
 								className={`w-fit rounded-[10px] shrink-0 px-4 py-3 min-h-[3.2vh] break-words ${
 									msg.role === "user"
 										? "self-end bg-[#1E4FE0]"
-										: "self-start bg-[#98caff] dark:bg-[#212121] text-[#141414] dark:text-[#fff]"
+										: "self-start bg-[#b5b5b5] dark:bg-[#212121] text-[#141414] dark:text-[#fff]"
 								} ${
 									msg.type === "document_fields"
 										? "!max-w-[500px] w-full"
