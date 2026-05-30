08a1435ccc4237cd62ab1f49620ab872564b0a6c
madiiikkk <madik2212@gmail.com>
2026-05-30 19:43:08 +0500

    themee v13

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -220,8 +220,8 @@ export default function Dialog({ messages, loading, isTyping, isStoped, onChoose
 								{msg.type === "article_win_chance" &&
 									msg.winRate !== undefined &&
 									msg.lossRate !== undefined && (
-										<div className="mt-4 p-5 bg-[#212121] border border-[#2A2A2A] rounded-[15px] w-full">
-											<h3 className="text-white font-medium mb-4 text-[16px]">
+										<div className="mt-4 p-5 bg-[#d8d8d8] dark:bg-[#212121] border border-[#a3a3a3] dark:border-[#2A2A2A] rounded-[15px] w-full">
+											<h3 className=" dark:text-white font-medium mb-4 text-[16px]">
 												{msg.article || "Анализ судебной практики"}
 											</h3>
 
