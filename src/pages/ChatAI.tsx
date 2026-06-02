7902c307ebbe374cb5349522dfeaa4aa62e17843
madiiikkk <madik2212@gmail.com>
2026-06-03 03:51:08 +0500

    LOADING AFTER LOGIN v3

--- src/pages/ChatAI.tsx
+++ src/pages/ChatAI.tsx
@@ -7,31 +7,21 @@ export default function ChatAI() {
 	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
-		// Проверяем, показывали ли мы уже загрузку в этой сессии
-		const hasLoadedBefore = sessionStorage.getItem("chatLoaded");
-
-		if (!hasLoadedBefore) {
-			// Если нет - ждем 2 секунды, затем показываем чат
-			const timer = setTimeout(() => {
-				setIsLoading(false);
-				sessionStorage.setItem("chatLoaded", "true");
-			}, 2000);
-			return () => clearTimeout(timer);
-		} else {
-			// Если уже показывали (например, при обновлении страницы), грузим сразу
+		const timer = setTimeout(() => {
 			setIsLoading(false);
-		}
+		}, 3000);
+
+		return () => clearTimeout(timer);
 	}, []);
 
 	if (isLoading) {
 		return (
 			<div className="flex flex-col h-[100vh] w-full items-center justify-center bg-black">
-				<h2 className="text-[#ffffff] text-[50px] font-bold tracking-[-0.5px] ">LAWLY</h2>
+				<h2 className="text-[#ffffff] text-[50px] font-bold tracking-[-0.5px]">LAWLY</h2>
 			</div>
 		);
 	}
 
-	// Основной интерфейс
 	return (
 		<div className="flex lg:flex h-[100vh] w-full">
 			<Sidebar refreshChats={refreshChats} />
