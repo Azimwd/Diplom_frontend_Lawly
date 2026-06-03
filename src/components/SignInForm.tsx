347f432f2b94710eec8cbd2fa16f30c6b984a99d
madiiikkk <madik2212@gmail.com>
2026-06-04 03:03:04 +0500

    ssssssssdsasdadasdawdasdsaasdasdsadawdasdsaddasdasdadsad auth

--- src/components/SignInForm.tsx
+++ src/components/SignInForm.tsx
@@ -5,7 +5,6 @@ import { useUser } from "../context/UserContext";
 import googleicon from "../assets/search.svg";
 import AuthInput from "../ui/AuthInput";
 import React, { useState } from "react";
-import GoogleCallbackPage from "./GoogleCallbackPage";
 
 interface PropsSignIn {
 	path: string;
@@ -29,7 +28,7 @@ export default function SignInForm({ path }: PropsSignIn) {
 
 	const handleClick = () => {
 		sessionStorage.setItem("showSplash", "true");
-		navigate("/auth/google/callback");
+		window.location.href = "https://api.yourdomain.com/users/google/login/";
 	};
 
 	const handleSubmit = async (e: React.FormEvent) => {
